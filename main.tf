8b129140f083c2def0c8ba017fa7f1c5323fa41f
ashok kumar <yenninti.kumar@walmart.com>
2025-10-15 19:58:56 +0530

    dummycommit

--- main.tf
+++ main.tf
@@ -24,21 +24,24 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+  
 }
 
 resource "aws_s3_bucket_policy" "public_policy" {
   bucket = aws_s3_bucket.static_site.id
 
   policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = "*"
-      Action = "s3:GetObject"
-      Resource = "${aws_s3_bucket.static_site.arn}/*"
+    Version = "2012-10-17",
+    Statement: [{
+      Sid       = "AllowPublicReadAccess",
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.static_site.arn}/*"
     }]
   })
 }
+
 
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.static_site.id
